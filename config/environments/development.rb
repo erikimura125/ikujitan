4d1efccaf4f17178dcbc34794979cb8b3f4f912d
erikimura125 <erikimura125@gmail.com>
2024-03-31 06:33:06 +0000

    [ADD]devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "27f1d1de5bbf4e47853f4bb5c3ceb4a0.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
+  config.active_job.queue_adapter = :inline
+  
 end
