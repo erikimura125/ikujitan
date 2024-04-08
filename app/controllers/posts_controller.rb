f56cbe3f7acd7a0bd2e37eed7473deeef9c3188f
erikimura125 <erikimura125@gmail.com>
2024-04-08 18:11:10 +0000

    [Add]index,show

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+  
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:image, :title, :body)
+  end
+  
+end
