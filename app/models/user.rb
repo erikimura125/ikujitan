b1d981b5e5e1aa6bb024b29ea87c1e5826653a8a
erikimura125 <erikimura125@gmail.com>
2024-05-27 00:20:58 +0000

    [Fix]test1

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,7 +64,6 @@ class User < ApplicationRecord
 
   def create_notification_follow!(current_user)
     temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ? ",current_user.id, id, 'follow'])
-    byebug
     if temp.blank?
       notification = current_user.active_notifications.new(
         visited_id: id,
