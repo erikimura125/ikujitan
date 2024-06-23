ca9190a9c52b39057e488a5e3760fcae135574d3
erikimura125 <erikimura125@gmail.com>
2024-06-23 04:43:21 +0000

    [FIX]notification-view2

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,16 +2,19 @@ class LikesController < ApplicationController
   
   def create
     @post_like = Like.new(user_id: current_user.id,post_id: params[:post_id])
-    @post_like.save
-    result = [done: "save",user_id: current_user.id, post_id: params[:post_id]]
-    @post_like.create_notification_like!(current_user)
-    redirect_to post_path(params[:post_id])
+    if @post_like.save
+    #result = [done: "save",user_id: current_user.id, post_id: params[:post_id]]
+      @post_like.create_notification_like!(current_user)
+      redirect_to post_path(params[:post_id]), notice: 'いいねしました'
+    else
+      redirect_to post_path(params[:post_id]), alert: 'いいねに失敗しました'
+    end
   end
   
   def destroy
     @post_like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
     @post_like.destroy
-    result = [done: "destroy",user_id: current_user.id, post_id:params[:post_id]]
+    #result = [done: "destroy",user_id: current_user.id, post_id:params[:post_id]]
     redirect_to post_path(params[:post_id])
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -6,10 +6,10 @@ class Like < ApplicationRecord
   validates :user_id, uniqueness: {scope: :post_id}
   
   def create_notification_like!(current_user)
-    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ? ", current_user.id, post.user_id, post_id, 'like'])
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ? ", current_user.id, post.user_id, post.id, 'like'])
     if temp.blank? && current_user.id != post.user_id
       notification = current_user.active_notifications.new(
-        post_id: id,
+        post_id: post.id,
         visited_id: post.user_id,
         action: 'like'
         )
